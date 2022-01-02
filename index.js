a845f8ccefab77ea8ba6567226948c8237d0c0c2
PrantaAcerjee <prantaacharjee647@gmail.com>
2022-01-02 18:05:30 +0600

    jwt verified

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // make public
 const express=require('express');
 const { MongoClient } = require('mongodb');
+const admin = require("firebase-admin");
 const ObjectId=require('mongodb').ObjectId;
 const app=express();
 const cors=require('cors');
@@ -10,17 +11,41 @@ require('dotenv').config();
 app.use(cors());
 app.use(express.json());
 
+ 
+const serviceAccount = require('./baby-walker-firebase-adminsdk.json');
+admin.initializeApp({
+credential: admin.credential.cert(serviceAccount)
+});
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rrj86.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
- async function run(){
- try{
+// verify token 
+async function verifyToken(req,res,next){
+if(req.headers?.authorization?.startsWith('Bearer ')){
+    const token=req.headers.authorization.split(' ')[1];
+    try{
+        const decodedUser=await admin.auth().verifyIdToken(token);
+        req.decodedEmail=decodedUser.email;
+
+    }
+    catch{
+
+    }
+}    
+next()
+}
+
+async function run(){
+try{
 await client.connect();
 console.log('database connected sucessfully')
 const database=client.db('babyWalker');
 const servicesCollection=database.collection('products');
 const reviewsCollection=database.collection('reviews');
+const usersCollection=database.collection('users');
 const orderCollection=database.collection('orders');
+
 // Get Api 
 app.get('/products',async (req,res)=>{
     const cursor=servicesCollection.find({});
@@ -43,6 +68,53 @@ const service=req.body;
 const result=await servicesCollection.insertOne(service)
 res.json(result);
 })
+// save users to DB
+    app.post('/users',async(req,res)=>{
+    const user=req.body;
+    const result=await usersCollection.insertOne(user)
+    res.json(result);
+})
+// user upsert to DB
+app.put('/users',async(req,res)=>{
+const user=req.body;
+const filter= {email:user.email}
+const options={upsert:true}
+const updateDoc={$set:user}
+const result=await usersCollection.updateOne(filter,updateDoc,options)
+res.json(result);
+});
+
+// make admin 
+app.put('/users/admin',verifyToken, async(req,res)=>{
+const user=req.body;
+const requester=req.decodedEmail;
+if(requester){
+    const requesterAccount=await usersCollection.findOne({email:requester})
+    if(requesterAccount.role==='admin'){
+        const filter={email:user.email};
+        const updateDoc={$set:{role:'admin'}};
+        const result=await usersCollection.updateOne(filter,updateDoc)
+        res.json(result);
+    }
+}else{
+    res.status(403).json({message:'you do not have access to make an admin'})
+}
+
+
+})
+
+// check is admin ?
+app.get('/users/:email',async(req,res)=>{
+const email=req.params.email;
+const query={email:email};
+const user=await usersCollection.findOne(query);
+let isAdmin=false
+if(user?.role==='admin'){
+    isAdmin=true
+}
+res.json({admin:isAdmin})
+})
+
 // post reviews api 
 app.post('/reviews',async(req,res)=>{
 const service=req.body;
